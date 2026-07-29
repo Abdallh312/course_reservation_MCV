59559cbcd181ede4b2dbac213213e01219dd32cb
Abdallh312 <abdallhshref4@gmail.com>
2026-07-29 21:12:17 +0300

    Lastone

--- Backend/DTOs/BuildingDto.cs
+++
@@ -1,9 +0,0 @@
-namespace Backend.DTOs
-{
-    // DTO for creating or updating a Building
-    public class CreateBuildingDto
-    {
-        public string Name { get; set; } = string.Empty;
-        public string? Address { get; set; }
-    }
-}
